c0bc281c1fb1aadfb462f2a8d0a71a66124107b7
ulascetinn <ulas.cetin.97@gmail.com>
2025-08-21 08:47:22 +0300

    ARV data(temsili)

---
+++ src/utils/arvCapacities.js
@@ -0,0 +1,34 @@
+// This file holds the sample data for ARV capacities.
+// Using named exports is a clean way to export multiple items from a single file.
+
+// Air capacities for brand1.
+export const arvBrand1 = [
+    { DN: 25, discharge1: 53, discharge2: 1, intake: 106 },
+    { DN: 40, discharge1: 135, discharge2: 1, intake: 270 },
+    { DN: 50, discharge1: 212, discharge2: 1, intake: 424 },
+    { DN: 65, discharge1: 358, discharge2: 1, intake: 716 },
+    { DN: 80, discharge1: 542, discharge2: 1, intake: 1084 },
+    { DN: 100, discharge1: 848, discharge2: 1, intake: 1696 },
+    { DN: 125, discharge1: 1325, discharge2: 1, intake: 2650 },
+    { DN: 150, discharge1: 1908, discharge2: 1, intake: 3816 },
+    { DN: 200, discharge1: 3392, discharge2: 1, intake: 6784 },
+    { DN: 250, discharge1: 5301, discharge2: 1, intake: 10602 },
+    { DN: 300, discharge1: 7634, discharge2: 1, intake: 15268 },
+
+];
+
+// Air capacities for brand2.
+export const arvBrand2 = [
+    { DN: 25, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 40, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 50, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 65, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 80, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 100, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 125, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 150, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 200, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 250, discharge1: 20, discharge2: 1, intake: 50 },
+    { DN: 300, discharge1: 20, discharge2: 1, intake: 50 },
+
+];
